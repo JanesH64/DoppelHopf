f9ccf7c0fcdfc5b890e5502b6c92974c1214915f
janeshorst <janes.horst64@gmail.com>
2023-10-06 17:21:30 +0200

    Init project

---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Weclome to Doppelhopf</h1>
+</template>
---
+++ app/pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1>Doppelhopf</h1>
+      <button v-on:click="login()">Test</button>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+    const supabase = useSupabaseClient();
+    const login = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    });
+  
+    if (error) {
+      console.error(error);
+    }
+  };
+  </script>
+  
