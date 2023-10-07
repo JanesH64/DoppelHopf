962f2b09b3906895b48350d6ff7f7c63872f6a1b
janeshorst <janes.horst64@gmail.com>
2023-10-07 15:54:46 +0200

    PWA Support
    TODO: Splash screen

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,14 +1,60 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     '@nuxtjs/supabase',
+    '@vite-pwa/nuxt'
   ],
   css: ['~/assets/css/tailwind.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Doppelhopf',
+      short_name: 'Doppelhopf',
+      theme_color: '#ffffff',
+      display: 'standalone',
+      icons: [
+        {
+          src: 'manifest-icon-192.maskable.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'manifest-icon-192.maskable.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable'
+        },
+        {
+          src: 'manifest-icon-512.maskable.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'manifest-icon-512.maskable.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    },
+    client: {
+      installPrompt: true,
+      periodicSyncForUpdates: 3600,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
     },
   },
 })
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Doppelhopf</h1>
-        <p class="py-6">Welcome to Doppelhopf. This is your place to brew your luck.</p>
-        <button class="btn btn-primary" v-on:click="login()">
-          <img class="h-6 w-6" src="/google-logo.png" alt="">
-          Login with Google
-        </button>
+      <div class="flex flex-col items-center">
+        <img src="/icon.png" class="h-2/5 w-2/5 m-6" alt="">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Doppelhopf</h1>
+          <p class="py-6 flex flex-col"><span>
+              Welcome to Doppelhopf.
+            </span>
+            <span>This is your place to brew your luck.</span>
+          </p>
+          <button class="btn btn-primary" v-on:click="login()">
+            <img class="h-6 w-6" src="/google-logo.png" alt="">
+            Login with Google
+          </button>
+        </div>
       </div>
     </div>
   </div>
