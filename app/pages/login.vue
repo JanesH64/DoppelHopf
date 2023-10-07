7af4c5d82fc60292fc353e8051c2f68af32df0b9
janeshorst <janes.horst64@gmail.com>
2023-10-07 11:37:11 +0200

    Login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
-    <h1>Doppelhopf</h1>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn" v-on:click="login()">Test</button>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Doppelhopf</h1>
+        <p class="py-6">Welcome to Doppelhopf. This is your place to brew your luck.</p>
+        <button class="btn btn-primary" v-on:click="login()">
+          <img class="h-6 w-6" src="/google-logo.png" alt="">
+          Login with Google
+        </button>
+      </div>
+    </div>
   </div>
 </template>
   
